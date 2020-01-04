64be1ae5ffde9772d8ee528b8481431764e6aa95
matildahmariwa <matildamariwa3@gmail.com>
2020-01-04 18:31:30 +0300

    exercise 1

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+Vue.component('message',{
+props:['title','body'],
+    data(){
+    return{
+        isVisible:true
+    }
+    },
+   template:
+       '<article class="message" v-show="isVisible">' +
+       ' <div class="message-header">' +
+       '{{title}}' +
+       '<button @click="hideModal" class="close">x</button>'+
+
+       '  </div>' +
+       ' <div class="message-body">' +
+       '{{body}}' +
+       '</div>' +
+       '</article>',
+methods:{
+    hideModal(){
+        this.isVisible=false
+    }
+}
+});
 
  Vue.component('task-list',{
    template:
