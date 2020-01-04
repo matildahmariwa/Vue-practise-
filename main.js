f42a77e2493b19b32e8b269e5a3db7f445a2c250
matildahmariwa <matildamariwa3@gmail.com>
2020-01-04 17:16:24 +0300

    components within components

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
+
+ Vue.component('task-list',{
+   template:
+       '<div><task v-for="task in tasks">{{task.task}}</task></div>',
+     data(){
+         return{
+             tasks:[
+                 {task:'Go to the store',completed:true},
+                 {task:'Finish screencast',completed:false},
+                 {task:'Make donation',completed:false},
+                 {task:'Clear inbox',completed:false},
+                 {task:'Make dinner',completed:false},
+                 {task:'Clean room',completed:true}
+
+             ]
+         };
+ }
+ });
+
  Vue.component('task',{
-template:'<li><slot></slot></li>'
-});
+     template:'<li><slot></slot></li>'
+ });
 
 new Vue({
     el:'#root'
