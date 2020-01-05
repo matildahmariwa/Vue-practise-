ddd4be2fa2076ca99a2d301aa7f8fe924610552a
matildahmariwa <matildamariwa3@gmail.com>
2020-01-05 19:57:34 +0300

    exercise2

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+Vue.component('tabs',{
+    template:'<div>' +
+        '<div class="tabs">' +
+        '<ul>' +
+        '<li class="is-active"><a>Pictures</a></li>'+
+        '<li><a>Music</a></li>' +
+        '<li><a>Videos</a></li>' +
+        '<li><a>Documents</a></li>' +
+        '</ul>' +
+        '</div>' +
+
+
+        '<div class="tabs-details">' +
+        '<slot></slot>' +
+        '</div>' +
+        '</div>',
+
+    mounted(){
+        console.log(this.$children);
+    }
+
+});
+vue.component('tab',{
+    template:'<div><slot></slot></div>'
+
+});
+
 Vue.component('message',{
 props:['title','body'],
     data(){
